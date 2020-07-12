e793504104eb87bc8756d5cd469e094dc689acb4
Ridho Saputra <ridhosptraa@student.telkomuniversity.ac.id>
2020-07-13 00:15:44 +0700

    Add verify token to block endpoint

--- app/middleware/verifyAuth.js
+++ app/middleware/verifyAuth.js
@@ -4,11 +4,12 @@ import {
 } from '../helpers/status';
 
 const verifyToken = async (req, res, next) => {
-    const { token } = req.headers;
-    if (!token) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
         errorMessage.error = 'Token not provided';
         return res.status(status.bad).send(errorMessage);
     }
+    const token = authHeader.split(' ')[1]
     try {
         const decoded = jwt.verify(token, process.env.SECRET_KEY);
         req.user = {
--- app/routers/blockRoute.js
+++ app/routers/blockRoute.js
@@ -1,10 +1,10 @@
 import express from 'express';
-
+import verifyToken from '../middleware/verifyAuth';
 import { createVote, getAllVote, } from '../controllers/blockController';
 
 const router = express.Router();
 
-router.get('/vote', getAllVote);
-router.post('/vote', createVote);
+router.get('/vote', verifyToken, getAllVote);
+router.post('/vote', verifyToken, createVote);
 
 export default router;
